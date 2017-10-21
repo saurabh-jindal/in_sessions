827776dce599c1770b64c9365b8cebb3120bb2c0
brute4s99 <piyushaggarwal002@gmail.com>
2017-10-22 00:06:14 +0530

    Auto Upload

---
+++ 1 - An intro/equidist.cpp
@@ -0,0 +1,10 @@
+// image of x2,y2 about x1,y1
+#include <iostream>
+using namespace std;
+int main(){
+	int x1,y1,x2,y2,x3,y3;
+	cin>>x1>>y1>>x2>>y2;
+	x3=x1-x2;
+	y3=y1-y2;
+	cout<<x3<<' '<<y3<<'\n';
+}
